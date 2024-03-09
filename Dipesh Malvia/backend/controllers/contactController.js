aff64b12cee2c03c867687b8805d94a862f6ce77
Haafii <hafismuhammedtk@gmail.com>
2024-03-09 20:53:15 +0530

    Add express-async-handler and mongodb dependencies, and update contactController.js

--- Dipesh Malvia/backend/controllers/contactController.js
+++ Dipesh Malvia/backend/controllers/contactController.js
@@ -1,36 +1,39 @@
-const getContacts = (req, res) => {
+const asyncHandler = require('express-async-handler');
+
+
+const getContacts = asyncHandler(async (req, res) => {
     res.send('Get all contacts');
-}
+})
 
 
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log(req.body);
-    const {name, email, phone} = req.body;
-    if(!name || !email || !phone){
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
         res.status(400);
         throw new Error('All fields must be filled');
     }
     res.send('Create new contact');
-}
+})
 
 
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.send(`get contact for ${req.params.id}`);
-}
+})
 
 
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.send(`update contact for ${req.params.id}`);
-}
+})
 
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.send(`Delete contact for ${req.params.id}`);
-}
+})
 
 
 
-module.exports = { getContacts, createContact, getContact, updateContact, deleteContact};
+module.exports = { getContacts, createContact, getContact, updateContact, deleteContact };
