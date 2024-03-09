e9dbc6564d9d98924ac2ab98938a420911edeb69
Haafii <hafismuhammedtk@gmail.com>
2024-03-09 12:32:43 +0530

    Add PATCH route for updating alien's subscription

--- Telusko/CRUD/routes/aliens.js
+++ Telusko/CRUD/routes/aliens.js
@@ -40,4 +40,18 @@ router.post('/', async (req, res)=>{
     }
 });
 
+
+router.patch('/:id', async (req, res) => {
+    try{
+        const alien = await Alien.findById(req.params.id)
+        alien.sub = req.body.sub
+        const a1 = await alien.save()
+        res.json(a1)
+    }catch(err){
+        res.send('Error' + err)
+    }
+
+})
+
+
 module.exports = router
