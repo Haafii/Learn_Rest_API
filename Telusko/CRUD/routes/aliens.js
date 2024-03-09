d3bfcdbfd1e6cba00139895ee08df2133f59d4cb
Haafii <hafismuhammedtk@gmail.com>
2024-03-09 12:16:08 +0530

    Add POST route for creating aliens

--- Telusko/CRUD/routes/aliens.js
+++ Telusko/CRUD/routes/aliens.js
@@ -7,10 +7,26 @@ router.get('/', async (req, res)=>{
         const aliens = await Alien.find()
         res.json(aliens)
     }
-    catch{
+    catch(err){
         res.send('Error ' + err)
     }
 });
 
+
+router.post('/', async (req, res)=>{
+    const alien = new Alien({
+        name: req.body.name,
+        tech: req.body.tech,
+        sub: req.body.sub
+    })
+
+    try{
+        const a1 =  await alien.save() 
+        res.json(a1)
+    }
+    catch(err){
+        res.send('Error')
+    }
+});
 
 module.exports = router
