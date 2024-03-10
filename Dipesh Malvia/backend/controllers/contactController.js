e4f0d8168b7109a476dd79c3b48c3cf1a5ac4737
Haafii <hafismuhammedtk@gmail.com>
2024-03-10 21:19:37 +0530

    Refactor contactController.js to include CRUD operations

--- Dipesh Malvia/backend/controllers/contactController.js
+++ Dipesh Malvia/backend/controllers/contactController.js
@@ -27,18 +27,41 @@ const createContact = asyncHandler(async (req, res) => {
 
 
 const getContact = asyncHandler(async (req, res) => {
-    res.send(`get contact for ${req.params.id}`);
+    const contact = await Contact.findById(req.params.id);
+    if(!contact) {
+        res.status(404);
+        throw new Error('Contact not found');
+    }
+    res.status(200).send(contact);
 })
 
 
 
 const updateContact = asyncHandler(async (req, res) => {
-    res.send(`update contact for ${req.params.id}`);
+    const contact = await Contact.findById(req.params.id);
+    if(!contact) {
+        res.status(404);
+        throw new Error('Contact not found');
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new: true,
+        }
+    )
+    res.status(200).send(updatedContact);
 })
 
 
 const deleteContact = asyncHandler(async (req, res) => {
-    res.send(`Delete contact for ${req.params.id}`);
+    const contact = await Contact.findById(req.params.id);
+    if(!contact) {
+        res.status(404);
+        throw new Error('Contact not found');
+    }
+    await contact.deleteOne()
+    res.status(200).send(contact);
 })
 
 
