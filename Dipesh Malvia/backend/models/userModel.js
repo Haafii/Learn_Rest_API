9f13a49a2ff827f0658fcbf93871407e2279a687
Haafii <hafismuhammedtk@gmail.com>
2024-03-10 22:11:08 +0530

    user schema created

---
+++ Dipesh Malvia/backend/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "email address already taken"]
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('User', userSchema);
