435e6a63d686cfe39a7cbc008928afdc69cbb285
Haafii <hafismuhammedtk@gmail.com>
2024-03-10 22:19:19 +0530

    Add bcrypt dependency and implement user registration

--- Dipesh Malvia/backend/controllers/userController.js
+++ Dipesh Malvia/backend/controllers/userController.js
@@ -1,6 +1,22 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
 
 const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error('Please fill all the fields');
+    }
+    const userAvailable = await User.findOne({email}) 
+    if (!userAvailable) {
+        res.status(400);
+        throw new Error('User already registered');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log(hashedPassword);
+
     res.json({message: 'Register the user '});
 })
 
